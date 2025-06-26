11083f783c28e1dac0b7bee60de688bcfb1c2bf2
Suprio-Das <priyamd507@gmail.com>
2025-06-26 22:34:11 +0600

    Navbar width set

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -3,8 +3,8 @@ import Logo from '../../assets/fragrance_fusion.png';
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm">
+        <div className='bg-base-100 shadow-sm'>
+            <div className="navbar w-[98%] mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">

669d62ae440166940ce5211b739793dfb9562935
Suprio-Das <priyamd507@gmail.com>
2025-06-27 22:22:15 +0600

    Womens component modified

--- src/Components/Perfumes/Womens.jsx
+++ src/Components/Perfumes/Womens.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 const Womans = () => {
     return (
         <div>
-            This is womans section.
+            This is Womens section.
         </div>
     );
 };

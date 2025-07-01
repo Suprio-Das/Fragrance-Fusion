c00dd912afb490dcc690c7d1da53dd61a3573402
Suprio-Das <priyamd507@gmail.com>
2025-07-01 22:56:28 +0600

    Hero title added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
         <div className="w-[94%] mx-auto">
-            <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md">
-
+            <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md flex p-11">
+                <h1 className="lg:text-5xl font-bold">Fragrance Fusion</h1>
             </header>
         </div>
     );

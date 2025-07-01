da8bec860c09491c5673d659641a05b5850b7a05
Suprio-Das <priyamd507@gmail.com>
2025-07-01 22:58:32 +0600

    Subtitle added to the hero banner

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,9 @@
 const Home = () => {
     return (
         <div className="w-[94%] mx-auto">
-            <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md flex p-11">
-                <h1 className="lg:text-5xl font-bold">Fragrance Fusion</h1>
+            <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md flex flex-col justify-center items-start p-11">
+                <h1 className="lg:text-5xl text-2xl font-bold">Fragrance Fusion</h1>
+                <p className="text-2xl">Elegance in Every Drop.</p>
             </header>
         </div>
     );

d3799d95ebfa44885dd2e00278990c61e49124cc
Suprio-Das <priyamd507@gmail.com>
2025-07-01 23:02:50 +0600

    Subtitle modified

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
         <div className="w-[94%] mx-auto">
             <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md flex flex-col justify-center items-start p-11">
                 <h1 className="lg:text-5xl text-2xl font-bold">Fragrance Fusion</h1>
-                <p className="text-2xl">Elegance in Every Drop.</p>
+                <p className="text-xl mt-2"><span className="bg-black py-1 px-2 rounded-sm text-white">Elegance</span> in Every Drop.</p>
             </header>
         </div>
     );

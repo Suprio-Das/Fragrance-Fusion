eccd1b0245917366d1137422de2e6c3f47aff5d4
Suprio-Das <priyamd507@gmail.com>
2025-07-01 23:08:07 +0600

    Button and Subtitle modified

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,8 @@ const Home = () => {
         <div className="w-[94%] mx-auto">
             <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md flex flex-col justify-center items-start p-11">
                 <h1 className="lg:text-5xl text-2xl font-bold">Fragrance Fusion</h1>
-                <p className="text-xl mt-2"><span className="bg-black py-1 px-2 rounded-sm text-white">Elegance</span> in Every Drop.</p>
-                <button className="primary-btn">Shop Now</button>
+                <p className="text-xl mt-2">Elegance in Every Drop.</p>
+                <button className="primary-btn btn mt-3">Shop Now</button>
             </header>
         </div>
     );

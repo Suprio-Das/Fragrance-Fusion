e5c7330f148df81e73d77eaf1bc186c38e044212
Suprio-Das <priyamd507@gmail.com>
2025-07-01 22:50:57 +0600

    Hero container styles added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div className="w-[94%] mx-auto">
-            <header className="hero">
+            <header className="hero min-h-[calc(100vh-100px)] mt-4 rounded-md">
 
             </header>
         </div>

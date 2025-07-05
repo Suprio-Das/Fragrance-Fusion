bda4244098f3cbc9b5bb6573662097fa27c5b1b9
Suprio-Das <priyamd507@gmail.com>
2025-07-05 23:07:40 +0600

    Card width set

--- src/Components/Shared/ProductCard.jsx
+++ src/Components/Shared/ProductCard.jsx
@@ -1,8 +1,8 @@
 const ProductCard = () => {
     return (
-        <div>
+        <div className="w-64">
             {/* Product Image */}
-            <div className="bg-[#f4f5f9]">
+            <div className="bg-[#f4f5f9] p-3">
                 <div className="border-1">
                     <img src='https://8ded8880.delivery.rocketcdn.me/themes/cosmecos-new/wp-content/uploads/2021/01/img_2-3-545x654.jpg' alt="" className="w-72" />
                 </div>

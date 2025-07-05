19e1cbb83b6a35b0ffd18c5c4a9142befaccb2e6
Suprio-Das <priyamd507@gmail.com>
2025-07-05 23:05:07 +0600

    Product image parent background color added

--- src/Components/Shared/ProductCard.jsx
+++ src/Components/Shared/ProductCard.jsx
@@ -2,7 +2,7 @@ const ProductCard = () => {
     return (
         <div>
             {/* Product Image */}
-            <div>
+            <div className="bg-[#f4f5f9]">
                 <div className="border-1">
                     <img src='https://8ded8880.delivery.rocketcdn.me/themes/cosmecos-new/wp-content/uploads/2021/01/img_2-3-545x654.jpg' alt="" className="w-72" />
                 </div>

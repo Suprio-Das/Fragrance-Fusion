f78d73425443099f3cbe2dff78d17c22cf90564b
Suprio-Das <priyamd507@gmail.com>
2025-07-05 23:21:44 +0600

    React Icons installed

--- src/Components/Shared/ProductCard.jsx
+++ src/Components/Shared/ProductCard.jsx
@@ -1,10 +1,10 @@
 const ProductCard = () => {
     return (
-        <div className="w-64">
+        <div className="w-64 product_card">
             {/* Product Image */}
             <div className="bg-[#f4f5f9] p-3">
                 <div className="border-1">
-                    <img src='https://8ded8880.delivery.rocketcdn.me/themes/cosmecos-new/wp-content/uploads/2021/01/img_2-3-545x654.jpg' alt="" className="w-72" />
+                    <img src='https://8ded8880.delivery.rocketcdn.me/themes/cosmecos-new/wp-content/uploads/2021/01/img_2-3-545x654.jpg' alt="" className="w-72 product_img" />
                 </div>
             </div>
         </div>

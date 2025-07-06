408a37375f8a6af5e3c8bf41035d999973d6ae6d
Suprio-Das <priyamd507@gmail.com>
2025-07-06 22:15:23 +0600

    fav pointer set to cursor

--- src/Components/Shared/ProductCard.jsx
+++ src/Components/Shared/ProductCard.jsx
@@ -12,8 +12,8 @@ const ProductCard = () => {
             <div className="bg-[#f4f5f9] p-3">
                 <div className="border-1 relative">
                     {
-                        favStatus === true ? <CiHeart className="text-3xl absolute right-1 top-1" onClick={AddToFavourite}></CiHeart> :
-                            <IoMdHeart className="text-2xl absolute right-1.5 top-1.5 text-dark" onClick={AddToFavourite}></IoMdHeart>
+                        favStatus === true ? <CiHeart className="text-3xl absolute right-1 top-1 cursor-pointer" onClick={AddToFavourite}></CiHeart> :
+                            <IoMdHeart className="text-2xl absolute right-1.5 top-1.5 text-dark cursor-pointer" onClick={AddToFavourite}></IoMdHeart>
                     }
                     <img src='https://8ded8880.delivery.rocketcdn.me/themes/cosmecos-new/wp-content/uploads/2021/01/img_2-3-545x654.jpg' alt="" className="w-72 product_img" />
                 </div>

9aa3b1700b23ef3b4cb01680030987795974398b
Suprio-Das <priyamd507@gmail.com>
2025-07-09 22:53:41 +0600

    buttons hover effect added

--- src/Components/Shared/ProductCard.jsx
+++ src/Components/Shared/ProductCard.jsx
@@ -22,8 +22,8 @@ const ProductCard = () => {
             <p className="text-lg font-semibold text-center">$49.99 <strike className="font-light ms-2">$69.99</strike></p>
             {/* Product Actions */}
             <div className="grid grid-cols-2 border-1 my-3">
-                <button className="btn border-r-1 border-r-black rounded-none">Add To Cart</button>
-                <button className="btn">Buy Now</button>
+                <button className="btn border-r-1 border-r-black rounded-none product-action-btn">Add To Cart</button>
+                <button className="btn product-action-btn">Buy Now</button>
             </div>
         </div>
     );

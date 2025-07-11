a940862ee3f358b3526eb49bcf6c3eadc0a8363c
Suprio-Das <priyamd507@gmail.com>
2025-07-11 22:31:49 +0600

    gap-y added to the small devices on the top product grid

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <section className="my-11">
                 <h1 className="text-2xl uppercase text-center">Client Selected</h1>
                 <h1 className="lg:text-5xl text-3xl font-bold uppercase text-center my-3">Top product</h1>
-                <div className="grid lg:grid-cols-3 grid-cols-1 justify-items-center my-11">
+                <div className="grid lg:grid-cols-3 grid-cols-1 gap-y-7 justify-items-center my-11">
                     <ProductCard></ProductCard>
                     <ProductCard></ProductCard>
                     <ProductCard></ProductCard>

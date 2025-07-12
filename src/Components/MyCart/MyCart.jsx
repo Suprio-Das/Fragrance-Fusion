44c5635123be8dbe06c7863276feb16616301f22
Suprio-Das <priyamd507@gmail.com>
2025-07-12 22:52:48 +0600

    grid sections added

--- src/Components/MyCart/MyCart.jsx
+++ src/Components/MyCart/MyCart.jsx
@@ -2,7 +2,14 @@ const MyCart = () => {
     return (
         <div className="w-[94%] mx-auto">
             <div className="grid lg:grid-cols-5 grid-cols-1">
-
+                {/* Product Section */}
+                <div>
+                    <h1>This is product section</h1>
+                </div>
+                {/* Price and Checkout section */}
+                <div>
+                    <h1>Price and Checkout section</h1>
+                </div>
             </div>
         </div>
     );

a5d27d3db5c933f3abc62f8cbc7035f78a673f3c
Suprio-Das <priyamd507@gmail.com>
2025-07-12 22:53:32 +0600

    colspan added

--- src/Components/MyCart/MyCart.jsx
+++ src/Components/MyCart/MyCart.jsx
@@ -3,11 +3,11 @@ const MyCart = () => {
         <div className="w-[94%] mx-auto">
             <div className="grid lg:grid-cols-5 grid-cols-1">
                 {/* Product Section */}
-                <div>
+                <div className="col-span-3">
                     <h1>This is product section</h1>
                 </div>
                 {/* Price and Checkout section */}
-                <div>
+                <div className="col-span-2">
                     <h1>Price and Checkout section</h1>
                 </div>
             </div>

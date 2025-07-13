62a1625c8de548f4cd7e1808622360f146948641
Suprio-Das <priyamd507@gmail.com>
2025-07-13 22:55:00 +0600

    My Cart Table added to the cart grid

--- src/Components/MyCart/MyCart.jsx
+++ src/Components/MyCart/MyCart.jsx
@@ -1,10 +1,12 @@
+import MyCartTable from "./MyCartTable";
+
 const MyCart = () => {
     return (
         <div className="w-[94%] mx-auto">
             <div className="grid lg:grid-cols-5 grid-cols-1">
                 {/* Product Section */}
                 <div className="col-span-3">
-                    <h1>This is product section</h1>
+                    <MyCartTable></MyCartTable>
                 </div>
                 {/* Price and Checkout section */}
                 <div className="col-span-2">

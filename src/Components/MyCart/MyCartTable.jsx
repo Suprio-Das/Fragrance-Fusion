ea14755751e15b4f2106b276c9d86b3f4bc68d51
Suprio-Das <priyamd507@gmail.com>
2025-07-15 21:13:23 +0600

    Quantity field updated

--- src/Components/MyCart/MyCartTable.jsx
+++ src/Components/MyCart/MyCartTable.jsx
@@ -23,11 +23,14 @@ const MyCartTable = () => {
                             </td>
                             <td>400.00</td>
                             <td>
-                                <div className="flex justify-center items-center">
-                                    <span className="border-2 border-gray-400 w-5 py-2 px-2 flex items-center justify-center hover:bg-black hover:text-white hover:border-1">-</span>
-                                    <input type="text" name="quantity" className="w-7 py-2 text-center outline-0 border-t-2 border-b-2 text-sm border-gray-400" />
-                                    <span className="border-2 border-gray-400 w-5 py-2 px-1 flex items-center justify-center hover:bg-black hover:text-white hover:border-1">+</span>
+                                <div className="join join-vertical lg:join-horizontal">
+                                    <button className="btn join-item">-</button>
+                                    <input className="input join-item w-11 text-center border-1 focus:outline-0 focus:border-gray-400"></input>
+                                    <button className="btn join-item">+</button>
                                 </div>
+                            </td>
+                            <td>
+
                             </td>
                         </tr>
                     </tbody>

dacfcbe22272c2dc31f74a7a393467b934d54bdd
Suprio-Das <priyamd507@gmail.com>
2025-07-16 22:11:08 +0600

    Heading modified

--- src/Components/MyCart/MyCart.jsx
+++ src/Components/MyCart/MyCart.jsx
@@ -4,7 +4,7 @@ import { FaCartShopping } from "react-icons/fa6"
 const MyCart = () => {
     return (
         <div className="w-[98%] mx-auto">
-            <h1 className="lg:text-4xl text-xl font-bold text-center my-5">My Cart <FaCartShopping></FaCartShopping> </h1>
+            <h1 className="lg:text-4xl text-xl font-bold my-5 flex items-center justify-center gap-2">My Cart <FaCartShopping></FaCartShopping> </h1>
             <div className="grid lg:grid-cols-5 grid-cols-1 gap-5 justify-items-center">
                 {/* Product Section */}
                 <div className="col-span-3 w-full">

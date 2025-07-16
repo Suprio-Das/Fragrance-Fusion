e8d4719b13efbfedb5c3d91a72cd9611a0d7a53b
Suprio-Das <priyamd507@gmail.com>
2025-07-16 22:10:08 +0600

    Cart icon added to the heading

--- src/Components/MyCart/MyCart.jsx
+++ src/Components/MyCart/MyCart.jsx
@@ -1,9 +1,10 @@
 import MyCartTable from "./MyCartTable";
+import { FaCartShopping } from "react-icons/fa6"
 
 const MyCart = () => {
     return (
         <div className="w-[98%] mx-auto">
-            <h1 className="lg:text-4xl text-xl font-bold text-center my-5">My Cart</h1>
+            <h1 className="lg:text-4xl text-xl font-bold text-center my-5">My Cart <FaCartShopping></FaCartShopping> </h1>
             <div className="grid lg:grid-cols-5 grid-cols-1 gap-5 justify-items-center">
                 {/* Product Section */}
                 <div className="col-span-3 w-full">

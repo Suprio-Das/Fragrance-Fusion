5fec6438a0380a23b439a60421a7bd64f9e497f6
Suprio-Das <priyamd507@gmail.com>
2025-07-17 22:37:21 +0600

    Cart totals width and price added

--- src/Components/MyCart/MyCart.jsx
+++ src/Components/MyCart/MyCart.jsx
@@ -11,10 +11,13 @@ const MyCart = () => {
                     <MyCartTable></MyCartTable>
                 </div>
                 {/* Price and Checkout section */}
-                <div className="lg:col-span-2">
+                <div className="lg:col-span-2 w-full">
                     <div className="border-2 border-gray-400 p-3">
                         <h1 className="lg:text-2xl font-semibold">Cart Totals</h1>
-                        <p className="text-xl font-semibold">Subtotal</p>
+                        <div className="flex justify-between items-center">
+                            <p className="text-xl font-semibold">Subtotal</p>
+                            <p>950.0Tk</p>
+                        </div>
                     </div>
                 </div>
             </div>

ecd226f431c4826a29dd9ec7556d4707f55579a7
Suprio-Das <priyamd507@gmail.com>
2025-07-20 22:12:24 +0600

    Two section created

--- src/Components/Perfumes/Mens.jsx
+++ src/Components/Perfumes/Mens.jsx
@@ -1,7 +1,16 @@
 const Mens = () => {
     return (
         <div className="w-[98%] mx-auto">
-            <h1>Mens Product Section.</h1>
+            <div>
+                {/* Range Section */}
+                <div>
+                    <h1>Product Range Section.</h1>
+                </div>
+                {/* Product Section */}
+                <div>
+                    <h1>Product Section.</h1>
+                </div>
+            </div>
         </div>
     );
 };

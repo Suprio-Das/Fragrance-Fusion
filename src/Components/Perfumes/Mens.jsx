8d2c2b2101cc2f05dbed2a87e380f562bb6879f8
Suprio-Das <priyamd507@gmail.com>
2025-07-20 22:25:21 +0600

    Layout modified

--- src/Components/Perfumes/Mens.jsx
+++ src/Components/Perfumes/Mens.jsx
@@ -3,16 +3,18 @@ import PageTitle from "../Shared/PageTitle";
 const Mens = () => {
     const title = "Mens";
     return (
-        <div className="w-[98%] mx-auto">
+        <div>
             <PageTitle title={title}></PageTitle>
-            <div className="grid lg:grid-cols-5 grid-cols-1">
-                {/* Range Section */}
-                <div className="col-span-1">
-                    <h1>Product Range Section.</h1>
-                </div>
-                {/* Product Section */}
-                <div className="col-span-4">
-                    <h1>Product Section.</h1>
+            <div className="w-[98%] mx-auto">
+                <div className="grid lg:grid-cols-5 grid-cols-1">
+                    {/* Range Section */}
+                    <div className="col-span-1">
+                        <h1>Product Range Section.</h1>
+                    </div>
+                    {/* Product Section */}
+                    <div className="col-span-4">
+                        <h1>Product Section.</h1>
+                    </div>
                 </div>
             </div>
         </div>

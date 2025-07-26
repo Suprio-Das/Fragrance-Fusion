986eb4d527c981ef56fbd072c5aa3c6655ec6dc1
Suprio-Das <priyamd507@gmail.com>
2025-07-26 20:51:19 +0600

    Footer services modified

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
             <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
                 <nav>
                     <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <a className="link link-hover">Mens Perfumes</a>
+                    <a className="link link-hover">Womens Perfumes</a>
+                    <a className="link link-hover">Fast Delivery</a>
+                    <a className="link link-hover">Top Products</a>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Company</h6>
